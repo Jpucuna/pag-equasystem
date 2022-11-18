2ad0294578c5751c53126a0bcfdca5116f878c4d
ivangkf <51732121+ivangkf@users.noreply.github.com>
2022-11-18 16:55:22 -0500

    estructura inicial de pag web

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-var words = ['Bienvenido a Equasystems', 'Desarrollo de software', 'Manejo de software', ' Paginas web', 'El mejor servicio te espera...'],
+//If you want to add more words, it must be done starting with a symbol or with - so that an empty div does not appear
+var words = ['-Bienvenido a Equasystems', '-Desarrollo de software', '-Manejo de software', '-Paginas web', '-El mejor servicio te espera...'],
     part,
     i = 0,
-    offset = 0,
+    offset = 1,
     len = words.length,
     forwards = true,
     skip_count = 0,
     skip_delay = 15,
-    speed = 70;
+    speed = 100;
 var wordflick = function () {
   setInterval(function () {
     if (forwards) {
@@ -22,13 +23,13 @@ var wordflick = function () {
       if (offset == 0) {
         forwards = true;
         i++;
-        offset = 0;
+        offset = 1;
         if (i >= len) {
           i = 0;
         }
       }
     }
-    part = words[i].substr(0, offset);
+    part = words[i].substr(1, offset);
     if (skip_count == 0) {
       if (forwards) {
         offset++;
